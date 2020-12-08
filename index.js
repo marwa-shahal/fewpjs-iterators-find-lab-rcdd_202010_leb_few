2bf259f084cf0c711b42ab98c82c114a386c7f8b
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-08 21:34:10 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+  if (record.find( rec => rec.result === "W"))
+  {return record.find( rec => rec.result === "W").year}
+  return undefined;
+  
+}
